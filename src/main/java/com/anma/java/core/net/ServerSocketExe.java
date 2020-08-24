74bb876722e60c241ffaf26e0ae6a1afe6554756
Andrii Maliuta <quadr988@gmail.com>
2020-08-24 19:08:14 +0300

    NET

--- src/main/java/com/anma/java/core/net/ServerSocketExe.java
+++ src/main/java/com/anma/java/core/net/ServerSocketExe.java
@@ -3,6 +3,7 @@ package com.anma.java.core.net;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketAddress;
 
 public class ServerSocketExe {
@@ -11,6 +12,8 @@ public class ServerSocketExe {
 
         ServerSocket serverSocket = new ServerSocket(8081);
 //        serverSocket.bind((SocketAddress) InetAddress.getByName(""), 1);
+        Socket socket = serverSocket.accept();
+
         serverSocket.getChannel();
         serverSocket.close();
     }
