24b6537073bfc5054c95ad45768e2dc9279ccd08
Andrii Maliuta <quadr988@gmail.com>
2020-09-12 12:24:49 +0300

    Json

--- src/main/java/com/anma/java/core/w3c/DocumentsExe.java
+++ src/main/java/com/anma/java/core/w3c/DocumentsExe.java
@@ -9,7 +9,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-public class DocuemntsExe {
+public class DocumentsExe {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -20,7 +20,7 @@ public class DocuemntsExe {
         Document document2 = builder.parse("https://www.codota.com/code/java/classes/org.w3c.dom.Document");
         System.out.println(document2.getDocumentElement().getAttribute("section"));
 
-        Document document3 = builder.parse(new ByteArrayInputStream("<html><head><title>This is title</title></head><>bodyThis is body</body></html>".getBytes()));
-        System.out.println(document3.getDocumentElement());
+//        Document document3 = builder.parse(new ByteArrayInputStream("<html><head><title>This is title</title></head><>bodyThis is body</body></html>".getBytes()));
+//        System.out.println(document3.getDocumentElement());
     }
 }
