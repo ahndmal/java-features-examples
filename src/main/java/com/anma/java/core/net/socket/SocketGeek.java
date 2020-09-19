243244407c717122570834e00ed49920849e90e9
Andrii Maliuta <quadr988@gmail.com>
2020-09-19 13:28:28 +0300

    Json

--- src/main/java/com/anma/java/core/net/socket/SocketGeek.java
+++ src/main/java/com/anma/java/core/net/socket/SocketGeek.java
@@ -7,31 +7,69 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SocketGeek {
 
-    public static void main(String[] args) throws IOException {
+    private static String WWW = "E:\\programming\\java\\projects\\java-example\\src\\resources\\www";
 
-        ServerSocket serverSocket = new ServerSocket(8080);
-        System.out.println("Server started!");
+    public static void main(String[] args) {
 
-        Socket client = serverSocket.accept();
-        System.out.println("New client connected!");
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Server started!");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+            while (true) {
 
-        PrintWriter writer = new PrintWriter(client.getOutputStream());
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected!");
 
-        while(!reader.ready());
-
-        while (reader.ready()) {
-            System.out.println(reader.readLine());
+                new Thread(() -> handleRequest(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
-        writer.println("HTTP/1.1 200 OK");
-        writer.println("Content-Type: text/html; charset=utf-8");
-        writer.println();
-        writer.println("<h1>Hello from Socket Client!</h1>");
-        writer.flush();
+    private static void handleRequest(Socket socket) {
+
+        try (BufferedReader input = new BufferedReader(
+                new InputStreamReader(
+                        socket.getInputStream(), StandardCharsets.UTF_8));
+             PrintWriter output = new PrintWriter(socket.getOutputStream())
+        ) {
+            while (!input.ready());
+
+            String firstLine = input.readLine();
+            String[] parts = firstLine.split(" ");
+            System.out.println(firstLine);
+
+            while (input.ready()) {
+                System.out.println(input.readLine());
+            }
+
+            Path path = Paths.get(WWW, parts[1]);
+
+            if (!Files.exists(path)) {
+                output.println("HTTP/1.1 404 NOT_FOUND");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<h1>Файл не найден!</h1>");
+                output.flush();
+                return;
+            }
+
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html; charset=utf-8");
+            output.println();
+
+            Files.newBufferedReader(path).transferTo(output);
+
+            System.out.println("Client disconnected!");
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
