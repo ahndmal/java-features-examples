c49a6bc3cdc6b525d015a2cb6f27a57e84180775
Andrii Maliuta <quadr988@gmail.com>
2020-11-02 16:39:13 +0200

    dates

--- src/main/java/com/anma/java/core/time/InstantExe.java
+++ src/main/java/com/anma/java/core/time/InstantExe.java
@@ -1,6 +1,7 @@
 package com.anma.java.core.time;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
@@ -8,8 +9,9 @@ public class InstantExe {
 
     public static void main(String[] args) {
 
-        Instant instant1 = Instant.from(LocalDateTime.of(2020,11,2,11,25));
+        Instant instant1 = Instant.from(LocalDateTime.of(2020,11,2,11,25, 11));
         Instant instant2 = instant1.plus(2,ChronoUnit.DAYS);
+        Instant instant3 = Instant.from(LocalDate.now());
 
         System.out.println(instant2);
     }
