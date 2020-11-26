e956e51b576f9d868f744f45061bbba273adaaed
Andrii Maliuta <quadr988@gmail.com>
2020-11-26 16:22:51 +0200

    Apache Http

---
+++ src/main/java/com/anma/java/http/apachehttp/ApacheHttpExe.java
@@ -0,0 +1,63 @@
+package com.anma.java.http.apachehttp;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApacheHttpExe {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+        get2();
+    }
+
+    private static void GET1() throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpGet httpGet = new HttpGet("https://pokeapi.co/api/v2/pokemon/ditto");
+
+        CloseableHttpResponse response1 = httpClient.execute(httpGet);
+
+        HttpEntity httpEntity = response1.getEntity();
+
+        System.out.println(httpEntity.getContentLength());
+    }
+
+    public static void get2() throws URISyntaxException {
+        URI uri = new URIBuilder()
+                .setScheme("http")
+                .setHost("www.google.com")
+                .setPath("/search")
+                .setParameter("q", "httpclient")
+                .setParameter("btnG", "Google Search")
+                .setParameter("aq", "f")
+                .setParameter("oq", "")
+                .build();
+        HttpGet httpget = new HttpGet(uri);
+        System.out.println(httpget.getURI());
+    }
+
+    public static void post() throws IOException {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://targethost/login");
+        List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+        nvps.add(new BasicNameValuePair("username", "vip"));
+        nvps.add(new BasicNameValuePair("password", "secret"));
+        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+        CloseableHttpResponse response2 = httpClient.execute(httpPost);
+    }
+}
