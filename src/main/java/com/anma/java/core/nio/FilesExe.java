87d72856dc38ccdbf554f182cf9e5104f5e94467
Andrii Maliuta <quadr988@gmail.com>
2020-11-30 19:44:15 +0200

    Files

--- src/main/java/com/anma/java/core/nio/FilesExe.java
+++ src/main/java/com/anma/java/core/nio/FilesExe.java
@@ -1,6 +1,6 @@
 package com.anma.java.core.nio;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
@@ -8,15 +8,39 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FilesExe {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
 
+        getFilesFromDir("src/resources");
+    }
 
+    private static void getFilesFromDir(String dir) throws IOException {
+
+        Stream<Path> files = Files.list(Path.of(dir));
+        files.forEach(System.out::println);
+    }
+
+    private static void readFile1() throws FileNotFoundException {
+
+        File file1 = new File("src/resources/text2-new.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file1));
+        try {
+            while (reader.ready()) {
+                System.out.println(reader.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void fileAttributes() throws IOException {
         BasicFileAttributes attributes = Files.readAttributes(Path.of("src/resources/text2-new.txt"), BasicFileAttributes.class);
         System.out.println(attributes.isOther());
-        System.out.println(attributes.isDirectory());;
+        System.out.println(attributes.isDirectory());
     }
 
     private static void misc() throws IOException, URISyntaxException {
