7ca0db7a27067079d19fa2d4a8757f9e9387389d
Andrii Maliuta <quadr988@gmail.com>
2020-12-02 12:29:54 +0200

    Http

--- src/main/java/com/anma/java/core/util/lists/ListsExec.java
+++ src/main/java/com/anma/java/core/util/lists/ListsExec.java
@@ -6,6 +6,11 @@ public class ListsExec {
 
     public static void main(String[] args) {
 
+        System.out.println(findDuplicates(Arrays.asList("Page1", "Page2", "Page3", "Page2", "Page4", "Page3", "Page5")));
+
+    }
+
+    private static void list3() {
         List empty1 = Collections.list(Collections.enumeration(Arrays.asList(0.5, 0.6)));
         empty1.add(0.7);
 
@@ -19,7 +24,6 @@ public class ListsExec {
         intList1.add(2);
         int freq = Collections.frequency(intList1, 1);
         System.out.println(freq);
-
     }
 
     private static void misc() {
@@ -43,5 +47,36 @@ public class ListsExec {
         System.out.println(deque.getLast());
         System.out.println(deque.pop());
         System.out.println(deque.element());
+    }
+
+//    public static void findDuplicates(List<String> pageTitles) {
+//
+//        Set<String> uniqueTitles = new HashSet<>();
+//        Set<String> duplicateTtiles = new HashSet<>();
+//
+//
+//
+//        for (String page : pageTitles) {
+//            if (!uniqueTitles.add(page)) {
+//                duplicateTtiles.add(page);
+//                System.out.println(page);
+//            }
+//        }
+//
+//        duplicateTtiles.forEach(System.out::println);
+//    }
+
+    private static  <T> Set<T> findDuplicates(Collection<T> collection) {
+
+        Set<T> duplicates = new LinkedHashSet<>();
+        Set<T> uniques = new HashSet<>();
+
+        for(T t : collection) {
+            if(!uniques.add(t)) {
+                duplicates.add(t);
+            }
+        }
+
+        return duplicates;
     }
 }
