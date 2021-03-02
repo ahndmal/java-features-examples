22dfeb246d55cfc01324eef601cbf515c2fccb40
Andrii Maliuta <quadr988@gmail.com>
2021-03-02 12:24:56 +0200

    JSON

--- src/main/java/com/anma/java/http/okhttp/OkHttpExe.java
+++ src/main/java/com/anma/java/http/okhttp/OkHttpExe.java
@@ -1,15 +1,21 @@
 package com.anma.java.http.okhttp;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class OkHttpExe {
+
     public static void main(String[] args) throws IOException {
 
-        testOkHttp("https://api.privatbank.ua/p24api/exchange_rates?json&date=10.04.2020");
+        LocalDate dateNow = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String todayDate = dateNow.format(formatter);
+        System.out.println("Today is " + todayDate);
+
+        testOkHttp("https://api.privatbank.ua/p24api/exchange_rates?json&date=" + todayDate);
 
     }
 
@@ -21,6 +27,13 @@ public class OkHttpExe {
 
         JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
 
-        System.out.println(jsonObject.get("username"));
+        // As Array
+        JsonArray array = jsonObject.getAsJsonArray("exchangeRate");
+        JsonObject element1 = array.get(0).getAsJsonObject();
+        System.out.println("saleRateNB = " + element1.get("saleRateNB"));
+        // As object
+//        JsonObject exchangeRate = jsonObject.getAsJsonObject("exchangeRate");
+//        System.out.println("saleRateNB = " + exchangeRate.get("saleRateNB"));
+
     }
 }
