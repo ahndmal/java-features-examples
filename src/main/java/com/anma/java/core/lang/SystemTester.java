3524658f2d935a0600d67234b58da3569ff1cc2f
Andrii Maliuta <quadr988@gmail.com>
2021-04-17 10:53:15 +0300

    Java System Props

--- src/main/java/com/anma/java/core/lang/SystemTester.java
+++ src/main/java/com/anma/java/core/lang/SystemTester.java
@@ -7,10 +7,26 @@ public class SystemTester {
 
     public static void main(String[] args) {
 
+        systemEnv();
+
+    }
+
+    private static void propsOne() {
+        //https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
+        Properties properties = System.getProperties();
+        System.out.println(properties.getProperty("os.name"));
+        System.out.println(properties.getProperty("os.arch"));
+        System.out.println(properties.getProperty("java.home"));
+    }
+
+    private static void systemEnv() {
         Map<String, String> envs = System.getenv();
         Properties properties = System.getProperties();
 
+        System.out.println("******* ENV: ");
         envs.entrySet().forEach(System.out::println);
+        System.out.println("******************************************");
+        System.out.println("******* Props: ");
         properties.entrySet().forEach(System.out::println);
     }
 }
