ef0b7773cbceb9065cba4a3c72e1a8c7aa20da44
Andrii Maliuta <quadr988@gmail.com>
2021-11-27 15:43:47 +0200

    regex

--- src/main/java/com/anma/java/exam/lambda/Lambda6.java
+++ src/main/java/com/anma/java/exam/lambda/Lambda6.java
@@ -1,11 +1,15 @@
 package com.anma.java.exam.lambda;
 
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 
 public class Lambda6 {
     public static void main(String[] args) {
 
-        BiConsumer biConsumer = (a,b) -> System.out.println(a);
-        biConsumer.accept(2,3);
+        BiConsumer<String, String> biConsumer = (a,b) -> System.out.println(a + " : " + b);
+        biConsumer.accept("2","3");
+        BiFunction<Integer, Integer, Integer> biFunction = (a, b) -> a * b;
+        BiPredicate<Integer, Long> biPredicate =
     }
 }
