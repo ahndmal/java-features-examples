455487d50c59766df7ad25e00597716a65f415db
Andrii maliuta <quadr988@gmail.com>
2021-12-21 19:24:56 +0200

    Files from URL

--- src/main/java/com/anma/java/core/math/MathMIsc.java
+++ src/main/java/com/anma/java/core/math/MathMIsc.java
@@ -4,6 +4,26 @@ public class MathMIsc {
 
     public static void main(String[] args) {
 
-        System.out.println(Math.round(0.66772342378 * 1000) / 1000F);
+        System.out.println(getCycleValue(2));
+        double x = (2 + (2 / 4)) + (2 / 9) + (2 / 16) + (2 / 25) + (2 / 36) + (2 / 49);
+        System.out.println(x);
+
+    }
+
+    public static double getCycleValue(double x) {
+
+        double s = 0;
+
+//        for (int i = 1; i <= 10; i++) {
+//            s += x + (x / (Math.pow(i, 2)));
+//        }
+
+        for (int i = 1; i <= 10; i++) {
+            x = x + (x / (Math.pow(i, 2)));
+            s += x;
+        }
+
+        return s;
+
     }
 }
