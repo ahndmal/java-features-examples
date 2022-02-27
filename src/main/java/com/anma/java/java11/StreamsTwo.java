71dea4c1bab9618be351861f0be3090840af85ce
andrii <quadr988@gmail.com>
2022-02-27 12:15:02 +0200

    StreamsTwo

---
+++ src/main/java/com/anma/java/java11/StreamsTwo.java
@@ -0,0 +1,10 @@
+package com.anma.java.java11;
+
+public class StreamsTwo {
+    public void testStreamsTwo() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/anma/java/java11/StreamsTwoTest.java
@@ -0,0 +1,16 @@
+package com.anma.java.java11;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StreamsTwoTest {
+
+    @Test
+    void testStreamsTwo() {
+    }
+
+    @Test
+    void main() {
+    }
+}
