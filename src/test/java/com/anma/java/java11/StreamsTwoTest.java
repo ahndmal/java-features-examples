99e8850998a8846dcb4c6124627cef24fa50f925
andrii <quadr988@gmail.com>
2022-02-27 14:20:00 +0200

    StreamsTwo

--- src/test/java/com/anma/java/java11/StreamsTwoTest.java
+++ src/test/java/com/anma/java/java11/StreamsTwoTest.java
@@ -2,12 +2,20 @@ package com.anma.java.java11;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StreamsTwoTest {
 
     @Test
     void testStreamsTwo() {
+        List<Integer> arr = new ArrayList<>();
+        int[] values = {1,2,3,4,5,6,7};
+        Arrays.stream(values).filter(x -> x % 2 == 0).forEach(arr::add);
     }
 
     @Test
