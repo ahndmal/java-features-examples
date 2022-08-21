7f32dd7c88c2a7413f8729c699128a109f993595
malandr <quadr988@gmail.com>
2022-08-21 13:24:57 +0300

    sync

--- src/main/java/com/anma/java/core/concur/vertxx/VrtHttpServer.java
+++ src/main/java/com/anma/java/core/concur/vertxx/VrtHttpServer.java
@@ -1,8 +1,10 @@
 package com.anma.java.core.concur.vertxx;
 
+import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.net.JksOptions;
 
 public class VrtHttpServer {
@@ -10,9 +12,49 @@ public class VrtHttpServer {
         Vertx vertx = Vertx.vertx();
         HttpServerOptions options = new HttpServerOptions()
                 .setMaxWebSocketFrameSize(1000000)
-                .setLogActivity(true);;
+                .setLogActivity(true);
+        ;
         HttpServer server = vertx.createHttpServer(options);
-        server.listen(7766, "localhost");
+
+        server.requestHandler(request -> {
+            String file = "";
+            System.out.println(request.path());
+            HttpServerResponse response = request.response();
+            var method = request.method().name();
+            if (method.equals("GET") && request.path().equals("/")) {
+                MultiMap headers = request.headers();
+                MultiMap params = request.params();
+                response.end("Hello world");
+            } else if (request.path().equals("/html")) {
+                MultiMap headers = request.response().headers();
+                headers.set("content-type", "text/html");
+                file = "src/resources/www/index.html";
+                request.response().sendFile(file);
+            } else if (request.path().equals("/cats")) {
+//                HttpServerResponse response = request.response();
+                response.setChunked(true);
+
+                MultiMap headers = response.headers();
+                headers.set("content-type", "application/json");
+                headers.set("other-header", "wibble");
+                response.putHeader("Content-Length", "300");
+
+//                request.response().write("cats");
+//                request.response().write("cats two", "UTF-16");
+                request.response().end("cats");
+            } else if (request.path().equals("/stream")) {
+                response.setChunked(true);
+                request.pipeTo(response);
+            }
+        });
+
+        server.listen(7766, "localhost", res -> {
+            if (res.succeeded()) {
+                System.out.println("Server is now listening!");
+            } else {
+                System.out.println("Failed to bind!");
+            }
+        });
 
     }
 }
