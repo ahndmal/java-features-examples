632ab1e451263262150a6a14d2d06e4d75426608
malandr <quadr988@gmail.com>
2022-10-21 20:40:17 +0300

    colored

--- src/main/java/com/anma/java/core/net/URLExe.java
+++ src/main/java/com/anma/java/core/net/URLExe.java
@@ -1,7 +1,13 @@
 package com.anma.java.core.net;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class URLExe {
 
@@ -12,8 +18,20 @@ public class URLExe {
         System.out.println(url.getPort());
         System.out.println(url.getHost());
         System.out.println(url.getPath());
-        System.out.println(url.getFile());
+//        System.out.println(url.getFile());
         System.out.println(url.getUserInfo());
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader((InputStream) url.getContent()));
+
+            URLConnection connection = url.openConnection();
+            BufferedReader reader1 = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            System.out.println(reader1.readLine());
+
+            reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
