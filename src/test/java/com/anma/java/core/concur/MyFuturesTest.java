a6ffde442bf9a960630b2ee544ec5ed31554881a
andrii <quadr988@gmail.com>
2023-06-15 22:26:22 +0300

    asn

--- src/test/java/com/anma/java/core/concur/MyFuturesTest.java
+++ src/test/java/com/anma/java/core/concur/MyFuturesTest.java
@@ -2,6 +2,9 @@ package com.anma.java.core.concur;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.concurrent.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,7 +22,7 @@ class MyFuturesTest {
     }
 
     @Test
-    void sub1() {
+    void pubs2() {
         Flow.Publisher publisher = new Flow.Publisher() {
             @Override
             public void subscribe(Flow.Subscriber subscriber) {
@@ -38,14 +41,35 @@ class MyFuturesTest {
                 subscribe(subscriber);
             }
         };
+    }
+
+    @Test
+    void sub1() {
+
         CompletableFuture completableFuture = CompletableFuture.runAsync(() -> {
             for (int i = 0; i < 100000; i++) {
                 long a = 12234243142431432L;
-                long b = a / 12345 + 1034324 - 123123 * 2;
+                a = a  / 123 - 123134 + 123343;
+                long b = a / 12345 + 1034324 - 123123 * 2 / 45 / 2 * 40 * 2 / 12 / 2 * 10;
                 long c = a - b;
                 System.out.println(c);
             }
         });
         System.out.println(completableFuture.isDone());
+    }
+
+    @Test
+    void simpleSout() {
+        var start = System.currentTimeMillis();
+        var startMoment = LocalDateTime.now();
+        for (int i = 0; i < 100000; i++) {
+            long a = 12234243142431432L;
+            a = a  / 123 - 123134 + 123343;
+            long b = a / 12345 + 1034324 - 123123 * 2 / 45 / 2 * 40 * 2 / 12 / 2 * 10;
+            long c = a - b;
+            System.out.println(c);
+        }
+        var end = Duration.between(startMoment, LocalDateTime.now()).toMillis();
+        System.out.println(end);
     }
 }
