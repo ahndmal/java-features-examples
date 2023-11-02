654e5af39067f7c08ac2c5c3076041e4128c5c6a
andrii <quadr988@gmail.com>
2023-11-02 20:08:27 +0200

    21

---
+++ src/main/java/com/anma/java/core/nio/DiskUsage.java
@@ -0,0 +1,36 @@
+package com.anma.java.core.nio;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class DiskUsage {
+    static final long K = 1024;
+
+    static void printFileStore(FileStore store) throws IOException {
+        long total = store.getTotalSpace() / K;
+        long used = (store.getTotalSpace() - store.getUnallocatedSpace()) / K;
+        long avail = store.getUsableSpace() / K;
+
+        String s = store.toString();
+        if (s.length() > 20) {
+            System.out.println(s);
+            s = "";
+        }
+        System.out.format("%-20s %12d %12d %12d\n", s, total, used, avail);
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.format("%-20s %12s %12s %12s\n", "Filesystem", "kbytes", "used", "avail");
+        if (args.length == 0) {
+            FileSystem fs = FileSystems.getDefault();
+            for (FileStore store: fs.getFileStores()) {
+                printFileStore(store);
+            }
+        } else {
+            for (String file: args) {
+                FileStore store = Files.getFileStore(Paths.get(file));
+                printFileStore(store);
+            }
+        }
+    }
+}
